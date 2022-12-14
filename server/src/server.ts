6d5c523a16ca9a06f3ff54ba2745a655cdf95b38
ViniDevBR <vinigtr386@gmail.com>
2022-12-13 22:55:54 -0300

    :fire: Using routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import { router } from './router'
+import path from 'node:path'
+
 
 //npm run dev and conect to mongoDB Compass
 mongoose.connect('mongodb://localhost:27017')
@@ -8,5 +11,8 @@ mongoose.connect('mongodb://localhost:27017')
     app.listen(4444, () => {
       console.log('SERVER ONLINE MONSTÃO')
     })
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+    app.use(express.json())
+    app.use(router)
   })
   .catch(() => console.log('MONGO FALHOU'))
