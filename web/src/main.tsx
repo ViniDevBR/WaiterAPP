9ce4fb8944c31130fc8805db156772f56ef18034
ViniDevBR <vinigtr386@gmail.com>
2022-12-14 19:31:06 -0300

    :fire: New files config

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,13 @@
+//REACT
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { App } from './App'
+//STYLED COMPONENTS
+import { GlobalStyles } from './styles/globalStyles'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
