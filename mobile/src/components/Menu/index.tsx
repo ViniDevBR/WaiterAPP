84e647de01304c892f7673eabc200daaa890c9d1
ViniDevBR <vinigtr386@gmail.com>
2022-12-16 18:52:34 -0300

    :fire: New styles for menu

--- mobile/src/components/Menu/index.tsx
+++ mobile/src/components/Menu/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { FlatList } from 'react-native'
 import { products } from '../../mocks/products'
 import { Text } from '../Text'
-import { MenuContainer, Product, ProductDetails, Image } from './styles'
+import { MenuContainer, Product, ProductDetails, Image, Separator, AddCartButton } from './styles'
+import { formatCoin } from '../../utils/formatCoin'
+import { PlusCircle } from '../Icons/PlusCircle'
 
 export function Menu() {
   return (
@@ -10,6 +12,7 @@ export function Menu() {
       <FlatList
         data={products}
         contentContainerStyle={{paddingHorizontal: 24}}
+        ItemSeparatorComponent={Separator}
         style={{marginTop: 32}}
         keyExtractor={product => product._id}
         renderItem={({item: product}) => {
@@ -17,14 +20,21 @@ export function Menu() {
           return(
             <Product>
               <Image
-                source={{uri: `http://localhost:4444/uploads/${product.imagePath}` }}
+                source={{uri: `http://192.168.0.4:4444/uploads/${product.imagePath}` }}
+                defaultSource={{uri: `http://192.168.0.4:4444/uploads/${product.imagePath}`}}
               />
 
               <ProductDetails>
-                <Text>{product.name}</Text>
-                <Text>{product.description}</Text>
-                <Text>{product.price}</Text>
+                <Text weight='600'>{product.name}</Text>
+                <Text size={14} color='#666' style={{marginVertical: 8}}>
+                  {product.description}
+                </Text>
+                <Text size={14} color='#333' weight='600'>{formatCoin(product.price)}</Text>
               </ProductDetails>
+
+              <AddCartButton>
+                <PlusCircle />
+              </AddCartButton>
             </Product>
           )}}
       />
