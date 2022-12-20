01a2f991fa97d6d3a329ad1e35945a6677e77db0
ViniDevBR <vinigtr386@gmail.com>
2022-12-20 20:50:12 -0300

    :fire: New functions for Cart

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -7,9 +7,11 @@ import { Menu } from '../../components/Menu'
 import { Footer } from '../../components/Footer'
 import { Container } from './styles'
 import { Modal } from '../../components/Modal'
-import { CartItemProps } from '../../@types/Cart'
 import { Button } from '../../components/Button'
 import { Cart } from '../../components/Cart'
+//TYPES
+import { CartItemProps } from '../../@types/Cart'
+import { IProduct } from '../../@types/Product'
 
 
 export function Home() {
@@ -23,13 +25,39 @@ export function Home() {
   function handleCancelOrder() {
     setSelectedTable(null)
   }
+  function handleAddToCart(product: IProduct) {
+    if (selectedTable === null) {
+      setIsModalOpen(true)
+    }
+
+    setCartItem(prevState => {
+      const itemIndex = prevState.findIndex(cartItem => cartItem.product._id === product._id)
+
+      if(itemIndex < 0) {
+        return prevState.concat({
+          quantity: 1,
+          product,
+        })
+      }
+
+      const newCartItem = [...prevState]
+      const newItem = newCartItem[itemIndex]
+
+      newCartItem[itemIndex] = {
+        ...newItem,
+        quantity: newItem.quantity + 1
+      }
+
+      return newCartItem
+    })
+  }
 
   return (
     <>
       <Container>
         <Header selectedTable={selectedTable} cancelOrder={handleCancelOrder}/>
         <Categories />
-        <Menu />
+        <Menu onAddToCart={handleAddToCart}/>
       </Container>
 
       <Footer>
