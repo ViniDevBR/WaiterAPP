217b6b38814e50cff5385240d03b1936821aac5a
ViniDevBR <vinigtr386@gmail.com>
2022-12-21 23:10:21 -0300

    :fire: New condicional renderings

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 //REACT
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { ActivityIndicator } from 'react-native'
 //COMPONENTS
 import { Header } from '../../components/Header'
 import { Categories } from '../../components/Categories'
@@ -8,19 +9,27 @@ import { Footer } from '../../components/Footer'
 import { Modal } from '../../components/Modal'
 import { Button } from '../../components/Button'
 import { Cart } from '../../components/Cart'
+import { Text } from '../../components/Text'
+import { Empty } from '../../components/Icons/Empty'
 //STYLED
 import { Container, CenteredContainer } from './styles'
 //TYPES
 import { CartItemProps } from '../../@types/Cart'
 import { IProduct } from '../../@types/Product'
-import { ActivityIndicator } from 'react-native'
+import { CategoriesItem } from '../../@types/Category'
+//BACK END
+import { API } from '../../services/api'
+
 
 
 export function Home() {
   const [selectedTable, setSelectedTable] = useState<string | null>(null)
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   const [cartItem, setCartItem] = useState<CartItemProps[]>([])
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [categories, setCategories] = useState<CategoriesItem[]>([])
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [isLoadingProducts, setIsLoadingProducts] = useState<boolean>(false)
 
   function handleSavedTable(table: string) {
     setSelectedTable(table)
@@ -80,21 +89,64 @@ export function Home() {
     })
   }
 
+
+  async function handleSelectedCategory(categoryId: string) {
+    const route = categoryId === '' ? '/products' : `/categories/${categoryId}/products`
+
+    setIsLoadingProducts(true)
+
+    const { data } = await API.get(route)
+    setProducts(data)
+    setIsLoadingProducts(false)
+  }
+  useEffect(() => {
+    try {
+      Promise.all([
+        API.get('/products'),
+        API.get('/categories')
+      ])
+        .then(([productResponse, categoriesResponse]) => {
+          setCategories(categoriesResponse.data)
+          setProducts(productResponse.data)
+          setIsLoading(false)
+        })
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  },[])
+
   return (
     <>
       <Container>
         <Header selectedTable={selectedTable} cancelOrder={handleResetOrder}/>
 
-        {!isLoading && (
-          <>
-            <Categories />
-            <Menu onAddToCart={handleAddToCart}/>
-          </>
-        )}
-        {isLoading && (
+        {isLoading ? (
           <CenteredContainer>
             <ActivityIndicator size='large' color='#d73035'/>
           </CenteredContainer>
+        ):(
+          <>
+            <Categories categories={categories} onSelectCategory={handleSelectedCategory}/>
+            {isLoadingProducts ? (
+              <CenteredContainer>
+                <ActivityIndicator size='large' color='#d73035'/>
+              </CenteredContainer>
+            ) : (
+              <>
+                {products.length > 0 ? (
+                  <Menu onAddToCart={handleAddToCart} products={products}/>
+                ) : (
+                  <CenteredContainer>
+                    <Empty />
+                    <Text color='#666' style={{marginTop: 24}}>Nenhum produto foi encontrado!</Text>
+                  </CenteredContainer>
+                )}
+              </>
+            )}
+          </>
         )}
       </Container>
 
