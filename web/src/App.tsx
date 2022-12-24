f702ea72f2157be38a53eb4909603f179c1e9bf4
ViniDevBR <vinigtr386@gmail.com>
2022-12-24 20:32:26 -0300

    :fire: Using toastify

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from './components/Header'
 import { Orders } from './components/Orders'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export function App() {
   return (
     <>
+      <ToastContainer position='bottom-center'/>
       <Header />
       <Orders />
     </>
