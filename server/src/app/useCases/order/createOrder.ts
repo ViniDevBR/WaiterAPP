82cf57e89b7a9e3220fdfb135cb5a6dd19206e10
ViniDevBR <vinigtr386@gmail.com>
2022-12-27 20:06:51 -0300

    :fire: Config Socket.io and using on createOrder

--- server/src/app/useCases/order/createOrder.ts
+++ server/src/app/useCases/order/createOrder.ts
@@ -1,12 +1,15 @@
 import { Order } from '../../models/Order'
 import { Request, Response } from 'express'
+import { io } from '../../../server'
 
 export async function createOrder(req: Request, res: Response) {
   try {
     const { table, products } = req.body
 
     const order = await Order.create({ table, products })
+    const orderDetailsForSocket = await order.populate('products.product')
 
+    io.emit('newOrder', orderDetailsForSocket)
     res.status(201).json(order)
 
   } catch (error) {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,25 @@
 //EXPRESS & NODE
 import express from 'express'
 import path from 'node:path'
+import http from 'node:http'
 import { router } from './router'
 //MONGO
 import mongoose from 'mongoose'
+//WEB SOCKET
+import { Server } from 'socket.io'
+
 
 //npm run dev
+const app = express()
+const server = http.createServer(app)
+export const io = new Server(server)
+
+
 mongoose.set('strictQuery', true)
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express()
-    app.listen(4444, () => {
-      console.log('SERVER ONLINE MONSTÃO')
-    })
-
+    const port = 4444
+    
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*')
       res.setHeader('Access-Control-Allow-Methods', '*')
@@ -24,5 +30,9 @@ mongoose.connect('mongodb://localhost:27017')
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
     app.use(express.json())
     app.use(router)
+
+    server.listen(port, () => {
+      console.log('SERVER ONLINE MONSTÃO')
+    })
   })
   .catch(() => console.log('MONGO FALHOU'))
